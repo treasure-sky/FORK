3a2ec74a1ddf1d8bff9909a695840e32eee9687a
ryudain05 <dkdlsekdls@naver.com>
2023-08-01 17:52:22 +0900

    feat/간단한 MvcConfiguration설정

---
+++ AD-defender/src/main/java/ms/twentythree/addefender/config/MvcConfiguration.java
@@ -0,0 +1,17 @@
+package ms.twentythree.addefender.config;
+
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+}
