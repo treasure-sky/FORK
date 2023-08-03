d6dad4c13202d523e102b8e3de8e843823250252
treasure-sky <ych0486666@gmail.com>
2023-08-03 18:31:51 +0900

    fix/네이버 블로그와 타 블로그 크롤링 방식 수정

--- AD-defender/src/main/java/ms/twentythree/addefender/service/CrawlingService.java
+++ AD-defender/src/main/java/ms/twentythree/addefender/service/CrawlingService.java
@@ -12,9 +12,22 @@ public class CrawlingService {
 
     public Optional<String> extractContent(String url) {
         try {
-            String mainUrl = url.substring(8);
-            Document doc = Jsoup.connect("https://m."+mainUrl).get();
-            String responseText = doc.select("#viewTypeSelector > div > div.se-main-container").text();
+            String responseText;
+            String domain = url.substring(8);
+
+            // 네이버 블로그의 경우 모바일 사이트로 접속하여 본문만 가져옴
+            if (domain.startsWith("m.blog.naver.com") || domain.startsWith("blog.naver.com")) {
+                String baseUrl = domain.startsWith("m.blog.naver.com") ? url : "https://m." + domain;
+                Document doc = Jsoup.connect(baseUrl).get();
+                responseText = doc.select("#viewTypeSelector > div > div.se-main-container").text();
+            }
+
+            // 타 블로그는 body의 전체 텍스트를 가져옴
+            // 추후에 파싱 규칙 추가 요망
+            else{
+                Document doc = Jsoup.connect(url).get();
+                responseText = doc.body().text();
+            }
             return Optional.of(responseText);
         } catch (IOException e) {
             e.printStackTrace();
