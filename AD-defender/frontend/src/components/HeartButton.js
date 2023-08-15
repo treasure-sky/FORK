6f959605a6d94a9e72707367ff750aca22eb7b74
yeahzee0421 <ydw08250421@gmail.com>
2023-08-16 02:40:47 +0900

    upload frontend

--- AD-defender/frontend/src/components/HeartButton.js
+++ AD-defender/frontend/src/components/HeartButton.js
@@ -2,9 +2,8 @@ import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 import HeartFill from "../assets/heart_fill.png";
 import HeartOutline from "../assets/heart_outline.png";
-// import {AiFillHeart} from 'react-icons/ai';
-// import {AiOutlineHeart} from 'react-icons/ai';
 
+//좋아요 기능 있으면 어떨까 하고 슬쩍 넣어봄 
 const Heart = styled.img`
     width: 40px
 `;
